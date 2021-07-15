576eb522d1265d6a230e7648e3b23ebd172fa563
mariasalomevillarreal <msvillarreal2021@gmail.com>
2021-07-15 12:17:18 -0500

    validated data

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
 
 function filterByQuery (query, animalsArray) {
     let personalityTraitsArray = [];
@@ -47,6 +49,33 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body,  animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray}, null, 2)
+  );
+
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name != 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species!== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     console.log(req.query)
@@ -64,7 +93,25 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.post('/api/aimals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // id any data req.body is incorrect, send 400 error back 
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+  //add animal to json file and animals to array in this function
+  const animal = createNewAnimal(req.body, animal);
+
+  res.json(animal);
+}
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
-
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//partse incoming JSON data
+app.use(express.json());
